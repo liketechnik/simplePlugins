654cd16730a60394453b85d34a4aab7a44fc7ba8
Florian Warzecha <flowa2000@gmail.com>
2018-02-18 21:49:54 +0100

    added blueprint project for version 1.2.1-1

---
+++ src/main/java/PluginBlueprint.java
@@ -0,0 +1,13 @@
+import simplePlugins.plugins.api.Plugin;
+
+/**
+ * @author Florian Warzecha
+ * @version 1.0
+ * @date 18 of Februar 2018
+ */
+public class PluginBlueprint extends Plugin {
+    
+    public PluginBlueprint() {
+        super("PluginBlueprint");
+    }
+}
